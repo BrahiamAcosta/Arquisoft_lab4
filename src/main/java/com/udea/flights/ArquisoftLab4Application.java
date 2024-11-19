b3ec42b0b27bcd1d916149010ee40f2f547c5139
BrahiamAcosta <brahiam.acosta@udea.edu.co>
2024-11-19 00:58:44 -0500

    First Commit

---
+++ src/main/java/com/udea/flights/ArquisoftLab4Application.java
@@ -0,0 +1,13 @@
+package com.udea.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquisoftLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquisoftLab4Application.class, args);
+	}
+
+}
